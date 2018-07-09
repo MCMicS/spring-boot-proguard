f2d67cea6d325f318a1b50550dfa506e9954c97c
mcmics <MCMicS@users.noreply.github.com>
2018-07-09 09:44:19 +0200

    Initial commit

---
+++ src/main/java/de/mcmics/springbootproguard/SpringBootProguardApplication.java
@@ -0,0 +1,12 @@
+package de.mcmics.springbootproguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProguardApplication.class, args);
+	}
+}
